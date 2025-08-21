cbd32da64b233c5c8b08b9f7a883543425dd4cac
kenjiuno <ku@digitaldolphins.jp>
2025-08-21 16:17:11 +0900

    0.2

--- OutlookIMExToolsAddIn1/Properties/AssemblyInfo.cs
+++ OutlookIMExToolsAddIn1/Properties/AssemblyInfo.cs
@@ -34,4 +34,4 @@ using System.Security;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("0.1.0.0")]
+[assembly: AssemblyInformationalVersion("0.2.0.0")]
